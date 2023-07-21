b7403717cc55750a92681e56ab7b7548bb8d641b
Dima <aldmikon@gmail.com>
2023-07-21 16:16:20 +0800

    5. - Installing pillow - Adding fields into custom user model - Replacing admin.ModelAdmin with UserAdmin base class for our custom user admin class - Adding a fieldset "custom profile" into change detail form using fields of custom user model

--- users/models.py
+++ users/models.py
@@ -4,5 +4,48 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    
+    """Custom user model"""
 
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other")
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_CHINESE = "cn"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_CHINESE, "Chinese")
+    )
+
+    CURRENCY_USD = "USD"
+    CURRENCY_CNY = "CNY"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_CNY, "CNY")
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, 
+        max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, 
+        max_length=3, null=True, default="en"
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, 
+        max_length=3, default="USD", null=True
+    )
+    superhost = models.BooleanField(default=False)

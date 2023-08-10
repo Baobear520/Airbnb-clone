9c98e2be5e0f827143f34f5e2c3022f53471fee7
Dima <aldmikon@gmail.com>
2023-08-10 18:30:05 +0800

    15 - Creating the folders "avatars" and "room_photos" in "uploads" - Setting MEDIA_URL to serve media files - Configuring url conf to view the files

--- users/models.py
+++ users/models.py
@@ -33,7 +33,7 @@ class User(AbstractUser):
         (CURRENCY_CNY, "CNY")
     )
 
-    avatar = models.ImageField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='avatars',null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, 
         max_length=10, null=True, blank=True

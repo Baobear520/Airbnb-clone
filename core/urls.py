f84d0d015417498becef329953e1d3c769411b1c
Dima <aldmikon@gmail.com>
2023-10-05 15:29:39 +0800

    23.
    - Adding pagination to the home page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
-from django.urls import include, path
+from django.urls import path
 from rooms import views as room_views
 
 
 app_name = "core"
 
 urlpatterns = [
-    path("", room_views.all_rooms, name="home"),]
+    path("", room_views.all_rooms, name="home"),
+]
